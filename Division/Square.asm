fc43143375aafa9ccd6f5617b17edbd183e56d10
CaidenKehrer <CaidenKehrer@gmail.com>
2017-03-26 12:27:43 -0600

    Added division, Working on it still.

---
+++ Division/Square.asm
@@ -0,0 +1,9 @@
+	;This program takes two numbers and divides them.
+	;The outputs will be put into the memory at two different locations
+	;Outputs: Remainder, Answer
+	;Inputs: Divided, Divisor
+	.ORIG x0000
+
+	Halt
+DIVISOR 
+	.END
