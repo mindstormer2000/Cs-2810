2bf02a29323a31b01ae6c5d5c0c9c8a4c9d6aeb9
CaidenKehrer <CaidenKehrer@gmail.com>
2017-04-09 08:52:36 -0600

    HUGE UPDATE… See change log for details

--- Program.asm
+++ Program.asm
@@ -1,4 +1,43 @@
-.ORIG x3000    ; start address in memory
+.ORIG x3400    ; start address in memory
+;Output format
+	LEA R0 EXAMP
+	PUTS
 
+;Get X1
+	LEA R0 XONEP
+	PUTS
+	JSRR PROMPT
+	
+;Get Y1
+	LEA R0 YONEP
+	PUTS
+	JSRR PROMPT
+
+;Get X2
+	LEA R0 XTWOP
+	PUTS
+	JSRR PROMPT
+;Get Y2
+	LEA R0 YTWOP
+	PUTS
+	JSRR PROMPT
+;Subtract x1 from x2
+
+;Square above items
+;Subtract y1 from y2
+;Square above items
+;Add the above squares
+;Square root above answer
+;Output answer
 Halt    ; stop the program
+XONE	.FILL	 #0
+XTWO	.FILL	 #0
+YONE	.FILL	 #0
+YTWO	.FILL	 #0
+EXAMP	.STRINGZ "The format we are using for finding the distance is as follows\n SQRT((x2-x1)^2+(y2-y1)^2)\n"
+XONEP	.STRINGZ "Please input X1"
+XTWOP	.STRINGZ "Please input X2"
+YONEP	.STRINGZ "Please input Y1"
+YTWOP	.STRINGZ "Please input X1"
 .END
+
