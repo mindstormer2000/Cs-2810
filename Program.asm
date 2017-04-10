3a9a227fe697ecce7a6edcc13385505cd9bb1a25
CaidenKehrer <CaidenKehrer@gmail.com>
2017-04-10 06:22:15 -0600

    Added -It now goes to the prompt and stores what comes back in r3 Removed - Nothing Notes -  none

--- Program.asm
+++ Program.asm
@@ -1,32 +1,41 @@
 .ORIG x3400    ; start address in memory
 ;Output format
-	LEA R0 EXAMP
+	LEA R0, EXAMP
 	PUTS
 
 ;Get X1
-	LEA R0 XONEP
+	LEA R0, XONEP
 	PUTS
-	LD R6 PROMPT
+	LD R6, PROMPT
 	JSRR R6
-	ST R3 XONE
+	ST R3, XONE
 ;Get Y1
-	LEA R0 YONEP
+	LEA R0, YONEP
 	PUTS
-	LD R6 PROMPT
+	LD R6, PROMPT
 	JSRR R6
-
+	ST R3, YONE
 ;Get X2
-	LEA R0 XTWOP
+	LEA R0, XTWOP
 	PUTS
-	LD R6 PROMPT
+	LD R6, PROMPT
 	JSRR R6
+	ST R3, XTWO
 ;Get Y2
-	LEA R0 YTWOP
+	LEA R0, YTWOP
 	PUTS
-	LD R6 PROMPT
+	LD R6, PROMPT
 	JSRR R6
+	ST R3, YTWO
 ;Subtract x1 from x2
-
+	;Twos complement x1
+	LD R1, XONE
+	NOT R1, R1
+	ADD R1, R1, #1
+	;Add -x1 to x2
+	LD R2 XTWO
+	ADD R5, R1, R2
+	
 ;Square above items
 ;Subtract y1 from y2
 ;Square above items
