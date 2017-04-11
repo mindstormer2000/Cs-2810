07a5beec88c4a1a51e5ecdb3e7e95232a6c29963
AlexJackman <jackmanalex228@gmail.com>
2017-04-11 00:17:11 -0600

    HUGE update with the main frame. Haven't tested it yet, would like to figure out how to enter things first. I have included branches and loops that tests for digit numbers. This is what I have so far

--- Prompt/Prompt.asm
+++ Prompt/Prompt.asm
@@ -1,96 +1,101 @@
 
-	; Here's the solution for getting double digit results. I don't know how to get inputs of more than one digit however
-	; I also don't quite know what to input into the other functions and where they go
-	; Hopefully these instructions are a little help
+	.ORIG	x3000
 
-	.ORIG	x3100
+	
+	; JUST TO MAKE THINGS CLEAR AND SIMPLE...
+	; User input example: 123 = 49, 50, 51 = 0000000000110001, 0000000000110010, 0000000000110011
+	; R1 = (49-48) * 100 = 1 * 100
+	; R2 = (50-48) * 10  = 2 * 10
+	; R3 = (51-48) 	     = 3
+	; Input: 123 as separate characters
+	: Postcondition: input = (1 * 100) + (2 * 10) + 3 = 123 as an actual number (0000000001111011)
 
-	; These instructions are the promting part of the program where the user enter the values
-	; For now, they're just single digit values
 
-	LEA	R0, PRMT1	; "Enter the value for point A from 0 to 256: "
+	LEA	R0, PRMT	; "Enter a number from 0 to 256: "
 	PUTS
-	GETC			; get first number
-	; check to make sure it's a number
-	; check to see if it's a newline
-	; if it is a number
-	OUT			; print entered character in R0
-	; increment for character count
-	; check to see if it's three
-	ADD	R1, R0, 0	; put the first number in R1
 
-	LEA	R0, PRMT2	; "Enter the value for point B from 0 to 256: "
-	PUTS
-	GETC			; get second number
-	OUt			; print entered character in R0
-	ADD	R2, R0, 0	; put the second number in R2
+	GETC			; get the first character entered by the user (first digit)
+	OUT			; output character as it's being typed
+	ADD	R1, R0, 0	; put the first digit in R1
+	ADD	R1, R1, -48	; translates the character into an integer
+	ADD	R4, R4, #1	; increment R4 for digit count (1)
 
-	LEA	R0, RSLT	; "The distance between the two points is "
+	GETC			; get the second character entered by the user (second digit)
+	OUT			; output character as it's being typed
+	ADD	R2, R0, 0	; put the second digit in R2
+	ADD	R2, R2, -48	; translates the character into an integer
+	ADD	R4, R4, #1	; increment R4 for digit count (2)
 
-	; FOR INPUT
-	;
-	; TENS
-	; if the input is 10-19
-	;	load 10 if first digit is 1, add last digit of input to 10	->	TENS	.FILL 	10
-	; if the input is 20-29
-	;	load 20 if first digit is 2, add last digit of input to 20	->	TWEN	.FILL 	20
-	; if the input is 30-39
-	;	load 30 if first digit is 3, add last digit of input to 30	->	THIR	.FILL 	30
-	; if the input is 40-49
-	;	load 40 if first digit is 4, add last digit of input to 40	->	FOUR	.FILL	40
-	; if the input is 50-59
-	;	load 50 if first digit is 5, add last digit of input to 50	->	FIFT	.FILL	50
-	; if the input is 60-69
-	;	load 60 if first digit is 6, add last digit of input to 60	->	SIXT	.FILL	60
-	; if the input is 70-79
-	;	load 70 if first digit is 7, add last digit of input to 70	->	SEVE	.FILL	70
-	; if the input is 80-89
-	;	load 80 if first digit is 8, add last digit of input to 80	->	EIGH	.FILL	80
-	; if the input is 90-99
-	;	load 90 if first digit is 9, add last digit of input to 90	->	NINE	.FILL	90
-	;
-	; HUNDREADS
-	; if the input is 100-199
-	;	load 100 if the fisrt digit is 1, add 10-19 if the second digit is 1-9, and then add the last digit	->	ONEH	.FILL	100
-	; if the input is 200-256
-	;	load 200 if the fisrt digit is 2, add 10-19 if the second digit is 1-9, and then add the last digit	->	TWOH	.FILL	200
-	;
-	; FOR OUTPUT
-	;
-	; TENS
-	; if the result is >= 10...90 and < 20...100, subtract it by 10...90, place a 1...9 in front, and output the 1...9 and the result
-	;
-	; HUNDREADS
-	; if it's >= 100 or 200 and < 200 or 257, subtract by 100 or 200, jump the result to one of the TENS functions, and output a 1 or 2 in front
+	GETC			; get the third character entered by the user (third digit)
+	OUT			; output the character as it's being typed
+	ADD	R3, R0, 0	; put the third digit in R3
+	ADD	R3, R3, -48	; translates the character into an integer
+	ADD	R4, R4, #1	; increment R4 for digit count (3)
 
-	; use function for distance calculator		TEST WITH ADDING: ADD R0, R2, R1
 
-	; These instructions are the tricky part, this is how the result can print more than two digits.
-	; I tested this by adding two numbers and it worked, but that's all I have so far
+	; This is the part where 2-3 characters becomes a 2-3 digit number
 
-	LD	R3, nONE	; load -48 into R3
-	ADD	R0, R0, R3	; subtract 48 to get ASCII value, put it in R0
+	; If R4 is 1 when enter key pressed, R5 = R1
+	; If R4 is 2 when enter key pressed, R5 = (R1 * 10) + R2
+	; If R4 is 3 when enter key pressed, R5 = (R1 * 100) + (R2 * 10) + R3
 
-	LD	R4, nNINE	; load -57 into R4
-	ADD	R1, R0, R4	; subtract 57 to get ASCII value, put it in R1
+	; if (R4 == 1)************************************************
 
-	BRnz	DONE		; if the answer is a single digit, print
-	ADD	R2, R0, 0	; else, store the digits in R2
+	ADD	R4, R4, #-1		; if ((R4 - 1) == 0)
+	BRz	ONE_DIGIT
+	; BRz	ONED			; "The number is one digit" for test
 
-	LD	R0, pONE	; get 1, put it in front of R2
-	OUT
+ONE_DIGIT
+	ADD	R5, R5, R1		; R5 = R1
 
-	ADD	R0, R2, -10	; get the second digit by subtracting the number by 10, with 1 placed in front
-DONE	OUT
+	; if (R4 == 2)************************************************
+
+	ADD	R4, R4, #-2		; if ((R4 - 2) == 0)
+	BRz	TWO_DIGIT_R1x10
+	; BRz	TWOD			; "The number is two digits" for test
+
+TWO_DIGIT_R1x10				; (R1 * 10)
+	ADD	R1, R1, #1		; do {R2++}
+	LD	R0, #-10		; load -10 into R0
+	ADD	R1, R1, R0		; compute R1 - 10
+	BRn	TWO_DIGIT_R1x10		; while (R1 < 10)
+
+	ADD	R5, R1, R2		; R5 = (R1 * 10) + R2
+
+	; if (R4 == 3)************************************************
+
+	ADD	R4, R4, #-3	; if ((R4 - 3) == 0)
+	BRz	THREE_DIGIT_R1x100
+	BRz	THREE_DIGIT_R2x10
+	; BRz	THREED			; "The number is three digits" for test
+
+THREE_DIGIT_R1x100			; (R1 * 100)
+	ADD	R1, R1, #1		; do {R1++}
+	LD	R0, #-100		; load -100 into R0
+	ADD	R1, R1, R0		; compute R1 - 100
+	BRn	THREE_DIGIT_R1x100	; while (R1 < 100)
+
+THREE_DIGIT_R2x10			; (R2 * 10)
+	ADD	R2, R2, #1		; do {R2++}
+	LD	R0, #-10		; load -10 into R0
+	ADD	R2, R2, R0		; compute R2 - 10
+	BRn	THREE_DIGIT_R2x10	; while (R2 < 10)
+
+	ADD	R0, R1, R2		; R5 = (R1 * 100) + (R2 * 10) + R3
+	ADD	R5, R0, R3
 
 	HALT
 
-PRMT1	.stringz "\nEnter the value for point A from 0 to 256: "
-PRMT2	.stringz "\nEnter the value for point B from 0 to 256: "
-RSLT	.stringz "\nThe distance between the two points is "
 
-nONE	.FILL	-48	; the ASCII value for -1
-nNINE	.FILL	-57	; the ASCII value for -9
-pONE	.FILL	49	; the ASCII value for 1
+
+
+PRMT	.stringz "\nEnter a number from 0 to 256: "
+RSLT	.stringz "\nThe number you entered is "
+
+ONED	.stringz "\nThe number is one digit long"
+TWOD	.stringz "\nThe number is two digits long"
+THREED	.stringz "\nThe number is three digits long"
+
+pONE	.FILL	49
 
 	.END
