866d198b6ba3c9b4702a978efae77e52d9a0b90f
CaidenKehrer <CaidenKehrer@gmail.com>
2017-04-25 04:36:47 -0600

    Added - Removed - Notes - FIXED: root 2

--- Version 1.0/Square Root/Square Roots.asm
+++ Version 1.0/Square Root/Square Roots.asm
@@ -23,32 +23,36 @@
 	ADD	R7	R7,	#1
 	ST	R7	OUTSIDE
 	ADD	R7	R7,	#1
-	ST	R7	OUTSIDE
+	ST	R7	DIVISOR
 	AND 	R7,	R5,	#0
-START	LD  	R3 	INSIDE
-	LD	R2	DIVISOR
-	LD	R7	DIVJUMP
-	JSRR	R7
+	;ALL registers a cleared out
+START	LD  	R3 	INSIDE		;Load I
+	LD	R2	DIVISOR		;Load n
+	LD	R7	DIVJUMP		
+	JSRR	R7			;Divide the I/n
+	ADD	R3	R3	#0	;Grab Remainder
+	BRnp	UPONE			;Check if (I/n) = 0
+	ADD	R2	R2	#-1
+	BRz	GOBACK		;check if (I/n) = 1
+	ADD	R2	R2	#1
+	ST 	R2 	TESTTWO		;store I/n into F
+	LD	R3	TESTTWO		;Load F
+	LD	R2	DIVISOR		;Load n
+	LD	R7	DIVJUMP		
+	JSRR	R7			;Divide the F/n
 	ADD	R3	R3	#0
-	BRnp	UPONE
-	ST 	R2 	TESTTWO
-	LD	R3	TESTTWO
-	LD	R2	DIVISOR
-	LD	R7	DIVJUMP
-	JSRR	R7
-	ADD	R3	R3	#0
-	BRnp	UPONE
-	ST	R2	INSIDE
-	LD	R2	OUTSIDE	
-	LD	R1	DIVISOR	;counter
+	BRnp	UPONE			;Check if (F/n) = 0
+	ST	R2	INSIDE		;I = (F/n)
+	LD	R2	OUTSIDE		;
+	LD	R1	DIVISOR		;counter
 	AND	R4	R4	#0
 MULTY	ADD	R4	R4	R2
 	ADD	R1	R1	#-1
 	BRp	MULTY
 	ST	R4 	OUTSIDE
 	BRNZP	TEST
-
-UPONE	LD	R1	DIVISOR
+	
+UPONE	LD	R1	DIVISOR		;Increment divisor
 	ADD	R1	R1	#1
 	ST	R1	DIVISOR
 
@@ -70,7 +74,7 @@ ONE	AND	R1,	R5	#0
 ZERO	AND 	R1,	R5,	#0
 	ST	R1	INSIDE	
 	ST	R1	OUTSIDE
-	
+		
 GOBACK	AND 	R0,	R5,	#0
 	AND 	R1,	R5,	#0
 	AND 	R2,	R5,	#0
